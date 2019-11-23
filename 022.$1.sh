ea1c4d99611bbe06b76aeb63c58836008e5a4092
afra666 <48674984+afra666@users.noreply.github.com>
2019-11-23 20:39:13 +0800

    Create 022.$1.sh

---
+++ 022.$1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [[ $1 -eq 2 ]]; then
+    echo "二"
+else
+    echo "不是二"
+fi
+
+
+
+
+######################################
+#!/usr/bin/env bash
+if [[ $1 -eq 2 ]]; then
+    echo "二"
+else
+    echo "不是二"
+fi
