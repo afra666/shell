28bba377d5c85f597a6e57dd6ec7bc4bdca58b60
afra666 <48674984+afra666@users.noreply.github.com>
2019-11-23 20:39:39 +0800

    Update 022.$1.sh

--- 022.$1.sh
+++ 022.$1.sh
@@ -9,9 +9,7 @@ fi
 
 
 ######################################
-#!/usr/bin/env bash
-if [[ $1 -eq 2 ]]; then
-    echo "二"
-else
-    echo "不是二"
-fi
+u18@ubuntu:~/IdeaProjects/untitled$ sh a7.sh 3
+不是二
+u18@ubuntu:~/IdeaProjects/untitled$ sh a7.sh 2
+二
