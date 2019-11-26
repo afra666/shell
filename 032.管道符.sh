f135676e3f81253fbee180825ceceb32dc0278d2
afra666 <48674984+afra666@users.noreply.github.com>
2019-11-26 00:48:09 -0800

    Update 032.管道符.sh

--- 032.管道符.sh
+++ 032.管道符.sh
@@ -1,6 +1,13 @@
  #!/usr/bin/env bash
+ #将参数传到管道符后面
  cat /home/u18/hello.c | grep main
-
-
 #显示出main所在的行的内容out:
 #void main(){
+
+
+
+
+#统计最终出现main的行中的col列数：
+cat /home/u18/hello.c | grep main | wc -c
+#out：
+#13
